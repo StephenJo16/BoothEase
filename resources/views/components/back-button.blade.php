1ea27cda354c727076cebac252c8195e72db14d0
StephenJo16 <josteven459@gmail.com>
2025-09-09 09:49:03 +0700

    Update back button

--- resources/views/components/back-button.blade.php
+++ resources/views/components/back-button.blade.php
@@ -1,11 +1,9 @@
 <div class="absolute top-4 left-4 sm:top-6 sm:left-6 z-10">
     <button
         onclick="history.back()"
-        class="rounded-full flex items-center justify-center shadow-sm hover:bg-[#ff7700] hover:border-[#ff7700] transition-all duration-200 group"
-        aria-label="Go back">
+        class="group" aria-label="Go back">
         <svg
-            class="hover:cursor-pointer w-10 h-10 text-gray-600 group-hover:text-white transition-colors duration-200"
-            fill="none"
+            class="w-10 h-10 text-gray-400 group-hover:text-[#ff7700] transition-colors duration-200" fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg">
@@ -13,7 +11,7 @@
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
+                d="M10 19l-7-7m0 0l7-7m-7 7h18" />
         </svg>
     </button>
 </div>
